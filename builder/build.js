a47702ba35801c332ad1a00e31a254c389537865
pixels-bytes <adam@pixelsandbytes.co>
2016-11-14 16:00:31 +0100

    pull contenful data

--- builder/build.js
+++ builder/build.js
@@ -16,6 +16,7 @@
  * @requires contenful
  * @requires fp
  * @requires fs
+ * @requires rename
  * @requires site.json
  * @requires templates
  */
@@ -26,18 +27,43 @@
 const _          = require('./fp');
 const contentful = require('./contentful');
 const fs         = require('fs');
+const path       = require('./paths');
+const rename     = require('rename');
 const site       = require('../site.json');
 const template   = require('./templates');
 
 
-// MAIN
-const posts = { site: site, files: [] };
+// IO FILTH
+const saveFile = x => fs.writeFileSync(x.buildpath, x.template);
+const save = _.map(saveFile);
 
-contentful.getSpace().then((items) => {
-  items.forEach(i => {
-    posts.files.push(i.fields.title);
-    _.log(i.sys.contentType.sys.id);
+
+// CUSTOM FUNCTIONS FOR THIS PROBLEM
+const extract = x => x.fields;
+const genPath = x => {
+  x.buildpath = rename(path.BUILD + x.slug, {
+    extname: '.html'
   });
 
-  fs.writeFileSync('.build/index.html', template.INDEX(posts));
+  return x;
+};
+
+
+// THE COMPOSITIONS
+const getData = type => _.comp(_.map(extract), _.filter(i => i.sys.contentType.sys.id == type));
+const statiq = "";
+
+
+// MAIN PROGRAM
+contentful.getSpace().then((items) => {
+  const posts = getData('post')(items);
+  const pages = getData('page')(items);
+
+  _.log(pages);
+
+
+//  pages.forEach(p => _.log(p.fields));
+//  posts.forEach(p => _.log(p.fields.tags));
+
+//  fs.writeFileSync('.build/index.html', template.INDEX(posts));
 });
